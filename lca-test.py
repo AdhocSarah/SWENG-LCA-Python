924b9482bb9b63655a84f3c6ffe73719ba20b077
Sarah Klein <72134091+AdhocSarah@users.noreply.github.com>
2021-10-31 17:12:31 +0000

    Added DAG test cases, altered code to handle DAG

--- lca-test.py
+++ lca-test.py
@@ -2,39 +2,49 @@ import unittest
 from lca import Node, findLCA
 
 
-
-
 class LCATest(unittest.TestCase):
+    root = None
 
+    def setUp(self):
+        n1 = Node(1)
+        n2 = Node(2)
+        n3 = Node(3)
+        n4 = Node(4)
+        n5 = Node(5)
+        n6 = Node(6)
+        n7 = Node(7)
+        self.root = n1
+        n1.addChild(n2)
+        n1.addChild(n3)
+        n2.addChild(n4)
+        n2.addChild(n5)
+        n3.addChild(n6)
+        n3.addChild(n7)
 
     def test_empty(self):
         root = None
         self.assertEqual(-1, findLCA(root, 1, 2))
 
-
-
     def test_valid(self):
-        root = Node(1)
-        root.left = Node(2)
-        root.right = Node(3)
-        root.left.left = Node(4)
-        root.left.right = Node(5)
-        root.right.left = Node(6)
-        root.right.right = Node(7)
-        self.assertEqual(2, findLCA(root, 4, 5))
-        self.assertEqual(1, findLCA(root, 4, 6))
-        self.assertEqual(1, findLCA(root, 3, 4))
-        self.assertEqual(2, findLCA(root, 2, 4))
+        self.assertEqual(2, findLCA(self.root, 4, 5))
+        self.assertEqual(1, findLCA(self.root, 4, 6))
+        self.assertEqual(1, findLCA(self.root, 3, 4))
+        self.assertEqual(2, findLCA(self.root, 2, 4))
 
     def test_invalid(self):
-        root = Node(1)
-        root.left = Node(2)
-        root.right = Node(3)
-        root.left.left = Node(4)
-        root.left.right = Node(5)
-        root.right.left = Node(6)
-        root.right.right = Node(7)
-        self.assertEqual(-1, findLCA(root, 4, 8))
+        self.assertEqual(-1, findLCA(self.root, 4, 8))
+
+    def test_dag(self):
+        n9 = Node(9)
+        n10 = Node(10)
+        self.root.addChild(n9)
+        self.root.addChild(n10)
+        self.assertEqual(1, findLCA(self.root, 9, 10))
+        n9.addChild(self.root)
+        self.assertEqual(-1, findLCA(self.root, 1, 9))
+
+
+
 
 if __name__ == '__main__':
     unittest.main()
--- lca.py
+++ lca.py
@@ -7,30 +7,37 @@ class Node:
     # Constructor to create a new binary node
     def __init__(self, key):
         self.key = key
-        self.left = None
-        self.right = None
+        self.children = []
+
+    def addChild(self, child):
+        self.children.append(child)
+
 
 # Finds the path from root node to given root of the tree.
 # Stores the path in a list path[], returns true if path
 # exists otherwise false
-def findPath( root, path, k):
 
-    # Baes Case
+def findPath(root, path, k):
+    # Base Case
     if root is None:
         return False
 
     # Store this node is path vector. The node will be
     # removed if not in path from root to k
-    path.append(root.key)
+
+    if root.key in path:
+        return False
+    else:
+        path.append(root.key)
 
     # See if the k is same as root's key
-    if root.key == k :
+    if root.key == k:
         return True
 
     # Check if k is found in left or right sub-tree
-    if ((root.left != None and findPath(root.left, path, k)) or
-            (root.right!= None and findPath(root.right, path, k))):
-        return True
+    for child in root.children:
+        if findPath(child, path, k) == 1:
+            return True
 
     # If not present in subtree rooted with root, remove
     # root from path and return False
@@ -38,26 +45,45 @@ def findPath( root, path, k):
     path.pop()
     return False
 
+
+def isAcyclic(root):
+    if not root:
+        return True
+    if len(root.children) == 0:
+        return True
+    return isAcyclicRec(root, [])
+
+
+def isAcyclicRec(root, path):
+    if root in path:
+        return False
+    acyc = True
+    path.append(root)
+    for child in root.children:
+        if not isAcyclicRec(child, path):
+            acyc = False
+    return acyc
+
+
 # Returns LCA if node n1 , n2 are present in the given
 # binary tre otherwise return -1
 def findLCA(root, n1, n2):
+    if not isAcyclic(root):
+        return -1
 
-    # To store paths to n1 and n2 fromthe root
+    # To store paths to n1 and n2 from the root
     path1 = []
     path2 = []
 
     # Find paths from root to n1 and root to n2.
     # If either n1 or n2 is not present , return -1
-    if (not findPath(root, path1, n1) or not findPath(root, path2, n2)):
+    if not findPath(root, path1, n1) or not findPath(root, path2, n2):
         return -1
 
     # Compare the paths to get the first different value
     i = 0
-    while(i < len(path1) and i < len(path2)):
+    while i < len(path1) and i < len(path2):
         if path1[i] != path2[i]:
             break
         i += 1
-    return path1[i-1]
-
-
-# This code is contributed by Nikhil Kumar Singh(nickzuck_007)
+    return path1[i - 1]
